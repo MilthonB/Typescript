df8b19154353f8f4fa72f215291d75f7057808e1
Milthon <milthonBor@gmail.com>
2022-08-06 17:21:44 -0700

    módulos basicos

---
+++ 01-TypeScript-intro/src/bases/01-types.ts
@@ -0,0 +1,26 @@
+
+// Es un módulo cuando se exportan datos 
+
+
+// Leer y comprender todos los errores que puedan ocurrir 
+
+// Typescript infiere todos los tipos de vairables y constantes que se usan
+
+// Dos opciones de declaraciones y sus tipos de datos
+// Uno lo infiere y el otro los define 
+export const name = "Saul"
+export const name1: string = "Estafani"
+export const esvalido: boolean = true
+
+// Para poder exportar tu data de una lugar a otro del proyecto
+// Se requiere agregar la palabra clave export
+// ejemplo
+
+export const edad: number = 165
+
+
+// No colocar codigo que se ejecute cuando sea leeido  
+
+
+
+
--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter'
+import { name, edad } from "./bases/01-types";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -10,13 +11,15 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Vite + TypeScript ${name}</h1>
+    <h1>Mi nombre es: ${name} -  Mi edad es ${edad}</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
+    
   </div>
 `
 
